6ca29504a7508482e144dc676280581ef2ed4ba8
tuanhung96 <tuanhung.hn96@gmail.com>
2024-03-15 19:41:59 +0700

    update feign

--- springboot-crud/src/main/java/com/example/demo/utils/EmployeeFeignClient.java
+++ springboot-crud/src/main/java/com/example/demo/utils/EmployeeFeignClient.java
@@ -1,6 +1,5 @@
 package com.example.demo.utils;
 
-import com.example.demo.config.Config;
 import com.example.demo.entity.Employee;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
-@FeignClient(value = "feign-client", url = "http://localhost:8080/api", configuration = Config.class)
+@FeignClient(value = "feign-client", url = "http://localhost:8080/api")
 public interface EmployeeFeignClient {
     @GetMapping("/employees/{employeeId}")
     Employee findById(@PathVariable("employeeId") int employeeId);
